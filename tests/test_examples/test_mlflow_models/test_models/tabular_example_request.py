f058cdb3ab5e36d8c4d8e603c6cfeccfd686a30c
tatevik.gharagyozyan <tatevik.gharagyozyan@craftworks.at>
2021-09-09 13:21:41 +0200

    refactor(scripts): NAVIO-1461 replaces cripts.test._fetch_data() instead of tabular_example_request.py

--- tests/test_examples/test_mlflow_models/test_models/tabular_example_request.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-example_request = pd.DataFrame({
- 'sepal_length': [5.1],
- 'sepal_width': [3.5],
- 'petal_length': [1.4],
- 'petal_width': [0.2],
-})
--- tests/test_examples/test_mlflow_models/test_models/test_tabular_setup.py
+++ tests/test_examples/test_mlflow_models/test_models/test_tabular_setup.py
@@ -1,10 +1,15 @@
 import tempfile
 import mlflow.pyfunc
-
+import pandas as pd
 from models import tabular
-from test_examples.test_mlflow_models.test_models.tabular_example_request import example_request
+from scripts.test import _fetch_data
 
 PREDICTION = 'prediction'
+
+
+def _get_example_request_df(model_path):
+    data = _fetch_data(model_path)[0]
+    return pd.DataFrame(data['data'], columns=data['columns'])
 
 
 def test_setup_predict():
@@ -12,18 +17,19 @@ def test_setup_predict():
     Tests that setup stores a model that can be loaded by mlflow
     """
 
-    with tempfile.TemporaryDirectory() as model_path:        
+    with tempfile.TemporaryDirectory() as model_path:
         tabular.setup(with_data=False, with_oodd=False, explanations=None, path=model_path)
 
         model = mlflow.pyfunc.load_model(model_path)
 
-    model_input = example_request
+        model_input = _get_example_request_df(model_path)
+
     model_output = model.predict(model_input)
 
-    expecpted_keys = [PREDICTION]
+    expected_keys = [PREDICTION]
 
-    for key in expecpted_keys:
+    for key in expected_keys:
         assert key in model_output
 
-    for key in expecpted_keys:
+    for key in expected_keys:
         assert len(model_output[key]) == model_input.shape[0]
