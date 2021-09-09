51f4389e96c600316ec2f6d5644bfe3bd9dea175
tatevik.gharagyozyan <tatevik.gharagyozyan@craftworks.at>
2021-09-09 13:45:45 +0200

    fix(setup.py): NAVIO-1461 adds to package requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+with open('requirements.txt', 'r') as req_file:
+    requirements = req_file.read().splitlines()
 
 test_requirements = ['pytest>=3', ]
 
