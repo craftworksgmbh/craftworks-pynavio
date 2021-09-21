dbadf61cba35291e2d41d2dd3aacb9752d784af8
tatevik.gharagyozyan <tatevik.gharagyozyan@craftworks.at>
2021-09-21 16:59:34 +0200

    test(infer_dependencies): adds tests for infer_dependencies

---
+++ tests/test_pynavio/test_utils/infer_dependencies.py
@@ -0,0 +1,30 @@
+from pynavio.utils.infer_dependencies import (infer_external_dependencies,
+                                              read_requirements_txt)
+
+
+def test_read_requirements_txt(tmp_path):
+    fixture_text = '''### fixture requirements.txt
+
+                      pandas==1.2.4
+                      numpy
+    '''
+    file_path = 'requirements.txt'
+    with open(file_path, 'w') as f:
+        f.write(fixture_text)
+
+    assert read_requirements_txt(file_path) == ['pandas==1.2.4', 'numpy']
+
+
+def test_infer_external_dependencies():
+    import numpy
+    import pandas as pd
+    if False:
+        import mlflow
+        exec("exec('import sklearn')")
+
+    pip_requirements = infer_external_dependencies('.')
+
+    assert any('pandas' in item for item in pip_requirements)
+    assert any('numpy' in item for item in pip_requirements)
+    assert any('mlflow' in item for item in pip_requirements)
+    assert any('scikit_learn' in item for item in pip_requirements)
