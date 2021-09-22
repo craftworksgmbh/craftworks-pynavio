77fbe622c998865ed7d0350a598edde74eec1934
tatevik.gharagyozyan <tatevik.gharagyozyan@craftworks.at>
2021-09-22 11:55:03 +0200

    refactor(infer_dependencies): modes requirement.txt generation to a separate function, renames the argument of infer_external_dependencies to module_path

--- pynavio/utils/infer_dependencies.py
+++ pynavio/utils/infer_dependencies.py
@@ -7,7 +7,20 @@ from typing import List
 import pkg_resources
 
 
-def infer_external_dependencies(model_module_path: str) -> List[str]:
+def _generate_requirements_txt_file(requirements_txt_file, module_path):
+    yes = subprocess.Popen(('yes', 'N'), stdout=subprocess.PIPE)
+    assert Path(module_path).exists(), f"{module_path} does not exist"
+    result = subprocess.call(
+        ('pigar', '-P', f'{module_path}', '-p', f'{requirements_txt_file}',
+         '--without-referenced-comments'),
+        stdin=yes.stdout)
+    if result != 0:
+        logging.error(f"please create and provide requirements.txt, as there was an error using pigar" \
+                      f" to auto-generate requirements.txt")
+        raise AssertionError
+
+
+def infer_external_dependencies(module_path: str) -> List[str]:
     """
     infers pip requirement strings.
     known edge cases and limitations:
@@ -15,23 +28,12 @@ def infer_external_dependencies(model_module_path: str) -> List[str]:
     and would result in a broken conda env
      -it might not be able to detect all the required dependencies,
      in which case the user could append/extend the list manually
-    @param model_module_path:
+    @param module_path:
     @return: list of inferred pip requirements, e.g. ['mlflow==1.15.0', 'scikit_learn == 0.24.1']
     """
     with TemporaryDirectory() as tmp_dir:
         requirements_txt_file = Path(tmp_dir) / 'requirements.txt'
-
-        yes = subprocess.Popen(('yes', 'N'), stdout=subprocess.PIPE)
-        assert Path(
-            model_module_path).exists(), f"{model_module_path} does not exist"
-        result = subprocess.call(
-            ('pigar', '-P', f'{model_module_path}', '-p',
-             f'{requirements_txt_file}', '--without-referenced-comments'),
-            stdin=yes.stdout)
-        if result != 0:
-            logging.error(f"please create and provide requirements.txt, as there was an error using pigar" \
-                          f" to auto-generate requirements.txt")
-            raise AssertionError
+        _generate_requirements_txt_file(requirements_txt_file, module_path)
         requirements = read_requirements_txt(requirements_txt_file)
     return requirements
 
