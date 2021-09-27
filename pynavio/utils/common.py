e7928fb12f821b36476ce86fbbe63d371939090b
yuri-lifanov <65657809+yuri-lifanov@users.noreply.github.com>
2021-09-27 16:26:31 +0200

    feat(NAVIO-1464): handle data frame input for example request (#3)
    * feat(NAVIO-1464): handle data frame input for example request
    
    * feat(NAVIO-1464): add feature_columns argument
    
    * feat(NAVIO-1464): move test file + logic fixes
    
    * feat(NAVIO-1464): docstring for schema generation function
    
    * feat(NAVIO-1464): further test cases and fixes

--- pynavio/utils/common.py
+++ pynavio/utils/common.py
@@ -11,6 +11,7 @@ from types import ModuleType
 from typing import Any, Dict, List, Optional, Union
 
 import mlflow
+import pandas as pd
 import pip
 import yaml
 
@@ -196,10 +197,18 @@ def to_navio_mlflow(model: mlflow.pyfunc.PythonModel,
     return Path(path + '.zip')
 
 
-def make_example_request(row: Dict[str, Any],
+def make_example_request(data: Union[Dict[str, Any], pd.DataFrame],
                          target: str,
                          datetime_column: Optional[str] = None,
+                         feature_columns: Optional[List[str]] = None,
                          min_rows: Optional[int] = None) -> ExampleRequest:
+    """ Generates a request schema for a navio MLflow model from data
+    @param data: a sample of data to use for schema generation
+    @param target: name of the target column
+    @param datetime_column: name of the datetime column (for time series)
+    @param feature_columns: list of names of feature columns
+    @param min_rows: minimal number of rows of data the model expects
+    """
 
     assert target != datetime_column, \
         'Target column name must not be equal to that of datetime column'
@@ -214,16 +223,40 @@ def make_example_request(row: Dict[str, Any],
             "nullable": False
         }
 
+    if isinstance(data, pd.DataFrame):
+        assert data.shape[0] > 0, \
+            'Cannot generate example request from an empty data frame'
+        row = data.iloc[:1].to_dict(orient='records')[0]
+    else:
+        assert isinstance(data, dict), \
+            f'Expected data to be of type dict, got type {type(data)}'
+        row = data
+
+    if feature_columns is None:
+        feature_columns = set(row.keys()) - {target, datetime_column}
+        feature_columns = sorted(list(feature_columns))  # to ease testing
+        assert feature_columns, \
+            'Could not infer a valid set of feature columns based on the ' \
+            f'data columns: {list(row.keys())}, with target={target} and ' \
+            f'datetime_column={datetime_column}'
+
+    assert feature_columns, 'Empty feature column list is not allowed'
+
+    assert target not in feature_columns, \
+        f'Feature column list must not contain target column {target}'
+
+    assert datetime_column not in feature_columns, \
+        'Feature column list must not contain datetime '\
+        f'column {datetime_column}'
+
     example = {
-        "featureColumns": [
-            _column_spec(name)
-            for name in row.keys()
-            if name != target and name != datetime_column
-        ],
+        "featureColumns": [_column_spec(name) for name in feature_columns],
         "targetColumns": [_column_spec(target)]
     }
 
     if datetime_column is None:
+        assert min_rows is None, \
+            'min_rows is only allowed when a datetime column is specified'
         return example
 
     example['dateTimeColumn'] = _column_spec(datetime_column, 'timestamp')
