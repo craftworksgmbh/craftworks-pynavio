d5f47fd30301744003b11b8af4425cfdc373f8ad
tatevik.gharagyozyan <tatevik.gharagyozyan@craftworks.at>
2022-06-09 18:27:46 +0200

    test(pynavio):NAVIO-1872 adds a testcase

--- tests/test_pynavio/test_dependencies_file_only.py
+++ tests/test_pynavio/test_dependencies_file_only.py
@@ -4,8 +4,10 @@ from pynavio.dependencies import infer_external_dependencies
 
 def test_infer_external_dependencies_file_only():
     pip_requirements = infer_external_dependencies(Path(__file__), file_only=False)
-
+    pip_requirements_dir = infer_external_dependencies(Path(__file__).parent)
     pip_requirements_file_only = infer_external_dependencies(Path(__file__))
     assert pip_requirements_file_only == []
     assert any('pytest' in item for item in pip_requirements)
+    assert any('pytest' in item for item in pip_requirements_dir)
+
 
