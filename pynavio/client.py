7e12437d4e10ea8bd41be4f0a721a2dca26b23ae
yuri-lifanov <65657809+yuri-lifanov@users.noreply.github.com>
2022-07-13 10:08:17 +0200

    NAVIO-1937: API Client (#36)
    * feat(NAVIO-1937): api client - upload zip & csv
    
    * feat(NAVIO-1937): status getters + tests
    
    * feat(NAVIO-1937): update upload method + finish tests
    
    * feat(NAVIO-1937): fix retrain api call
    
    * feat(NAVIO-1937): fix lint + docstrings

---
+++ pynavio/client.py
@@ -0,0 +1,160 @@
+import json
+from pathlib import Path
+from typing import Union
+
+import requests
+from requests.adapters import HTTPAdapter, Retry
+
+
+class Client:
+    """ navio API client
+
+    Uploading data:
+
+        >>> client = Client('https://navio.craftworks.io/', 'abc123')
+        >>> client.upload_csv('./data.csv', 'workspace-id-1', 'my-data')
+
+    Uploading models:
+
+        >>> client = Client('https://navio.craftworks.io/', 'abc123')
+        >>> client.upload_model_zip(
+        ...     './model.zip', 'workspace-id-1',
+        ...     'use-case-id-1', 'my-model')
+    """
+
+    def __init__(self, navio_url: str, api_token: str) -> 'Client':
+        self._url = navio_url
+        self._token = api_token
+        self._session = self._create_session()
+        self._session.headers.update({"Authorization": f"Bearer {api_token}"})
+
+    @staticmethod
+    def _create_session() -> requests.Session:
+        """ Returns requests.Session configured with retries
+        :return: configured requests session
+        """
+        session = requests.Session()
+        retries = Retry(total=5,
+                        backoff_factor=0.1,
+                        status_forcelist=[500, 502, 503, 504])
+        session.mount('http://', HTTPAdapter(max_retries=retries))
+        session.mount('https://', HTTPAdapter(max_retries=retries))
+        return session
+
+    def _check_response(self, response: requests.Response) -> None:
+        """ Checks response status, raising the response content if needed
+        :param response: response object to check
+        :return: None
+        """
+        try:
+            response.raise_for_status()
+        except Exception as err:
+            raise type(err)(response.json()) from err
+
+    def assign_model_to_deployment(self, model_id: str,
+                                   deployment_id: str) -> None:
+        """ Assigns specified model to specified deployment
+
+        Checking for deployment status is not required. Deployment is only
+        usable if the model's status is "READY"
+
+        :param model_id: uuid of the model
+        :param deployment_id: uuid of the deployment
+        :return: None
+        """
+        suffix = f'/api/navio/v1/deployments/{deployment_id}/models/{model_id}'
+        response = self._session.put(f'{self._url}{suffix}')
+        self._check_response(response)
+
+    def get_dataset_status(self, dataset_id: str, workspace_id: str) -> str:
+        """ Returns status of the specified dataset in given workspace
+        :param dataset_id: the uuid of the dataset
+        :param workspace_id: the uuid of the workspace
+        :return: status string in ['PROCESSING', 'READY', 'FAILED']
+        """
+        suffix = (f'/api/navio/v1/workspaces/{workspace_id}'
+                  f'/datasets/file/{dataset_id}/status')
+        response = self._session.get(f'{self._url}{suffix}')
+        self._check_response(response)
+        return response.json()['value']
+
+    def get_model_status(self, model_id: str) -> str:
+        """ Returns status of the specified model
+        :param model_id: the uuid of the model
+        :return: status string in ['READY', 'DATASET_LOADING',
+        'WAITING_FOR_CLUSTER', 'WAITING_FOR_RESOURCES', 'TRAINING_STARTED',
+        'BUILDING_DOCKER_IMAGE', 'BUILDING_DOCKER_IMAGE_FAILED',
+        'TRAINING_FAILED']
+        """
+        suffix = f'/api/navio/v1/models/{model_id}/status'
+        response = self._session.get(f'{self._url}{suffix}')
+        self._check_response(response)
+        return response.json()['state']
+
+    def get_deployment_status(self, deployment_id: str) -> dict:
+        """ Returns status of the specified deployment
+        :param deployment_id: the uuid of the deployment
+        :return: status info as a dictionary
+        """
+        suffix = f'/api/navio/v1/deployments/{deployment_id}/status'
+        response = self._session.get(f'{self._url}{suffix}')
+        self._check_response(response)
+        return response.json()
+
+    def upload_model_zip(self, path: Union[Path, str], workspace_id: str,
+                         use_case_id: str, name: str) -> str:
+        """ Sends the model zip to navio. Returns model id
+        :param path: local path to the model archive
+        :param workspace_id: the uuid of the target workspace on navio
+        :param use_case_id: the uuid of the target use case on navio
+        :param name: the name to give the model
+        :return: uuid of the created navio model
+        """
+        path = Path(path)
+        suffix = (f'/api/navio/v1/models/workspaces/{workspace_id}'
+                  f'/usecases/{use_case_id}/upload')
+
+        dto_body = json.dumps({'name': name, 'description': 'description'})
+        with path.open('rb') as file:
+            # yapf: disable
+            files = [
+                ('createDTO', ('createDTO', dto_body, 'application/json')),
+                ('model', (path.name, file, 'multipart/form-data'))
+            ]
+            # yapf: enable
+            response = self._session.post(f'{self._url}{suffix}', files=files)
+        self._check_response(response)
+        return response.json()['id']
+
+    def upload_csv(self, path: Union[Path, str], workspace_id: str,
+                   name: str) -> str:
+        """ Sends the CSV file to navio. Returns dataset id
+        :param path: local path to the csv dataset
+        :param workspace_id: the uuid of the target workspace on navio
+        :param name: the name to give the dataset
+        :return: the uuid of the created navio dataset
+        """
+        suffix = f'/api/navio/v1/workspaces/{workspace_id}/datasets/file'
+        dto_body = json.dumps({'name': name, 'columnDelimiter': 'COMMA'})
+        with Path(path).open('rb') as file:
+            # yapf: disable
+            files = [
+                ('createDTO', ('createDTO', dto_body, 'application/json')),
+                ('file', ('file', file, 'multipart/form-data'))
+            ]
+            # yapf: enable
+            response = self._session.post(f'{self._url}{suffix}', files=files)
+        self._check_response(response)
+        return response.json()['id']
+
+    def retrain_model(self, model_id: str, dataset_id: str) -> str:
+        """ Starts the retraining of given model using given dataset
+        :param model_id: the uuid of the model to retrain
+        :param dataset_id: the uuid of the dataset to use for training
+        :return: the uuid of the newly created model
+        """
+        suffix = f'/api/navio/v1/models/{model_id}/train'
+        response = self._session.post(f'{self._url}{suffix}',
+                                      json={"datasetId": dataset_id})
+        self._check_response(response)
+        return response.json()['idOfNewModel']
---
+++ tests/test_pynavio/test_client.py
@@ -0,0 +1,182 @@
+import socket
+
+from contextlib import contextmanager, closing
+from pathlib import Path
+from queue import Queue
+from threading import Thread
+from typing import Generator
+from wsgiref.simple_server import make_server, WSGIRequestHandler
+
+from pynavio.client import Client
+
+import pytest
+
+
+def _find_free_port() -> int:
+    """ Use the OS to get an unused port
+    """
+    with closing(socket.socket()) as sock:
+        sock.bind(('', 0))
+        return sock.getsockname()[1]
+
+
+MOCK_SERVER_PORT = _find_free_port()
+MOCK_SERVER_URL = f'http://localhost:{MOCK_SERVER_PORT}'
+MOCK_SERVER_TOKEN = 'abc123'
+
+
+class QuietRequestHandler(WSGIRequestHandler):
+    def log_message(self, *args, **kwargs) -> None:
+        """ Empty so that the server doesn't log every request """
+        pass
+
+
+@contextmanager
+def _request_capture(response: str = '') -> Generator:
+    """ Accepts requests, pushing their payload onto the returned queue
+    :param response: what the server should respond with
+    :return: reference to the queue
+    """
+    queue = Queue()
+
+    def _handle(environ, start_response) -> list:
+        length = int(environ.get('CONTENT_LENGTH', '') or 0)
+        # yapf: disable
+        queue.put_nowait({
+            'content': environ['wsgi.input'].read(length),
+            'auth': environ.get('HTTP_AUTHORIZATION'),
+            'path': environ.get('PATH_INFO'),
+            'query': environ.get('QUERY_STRING')
+        })
+        # yapf: enable
+        start_response('200 OK', [('Content-type', 'text/plain')])
+        return [response.encode()]
+
+    _server = make_server('0.0.0.0', MOCK_SERVER_PORT, _handle,
+                          handler_class=QuietRequestHandler)
+    try:
+        thread = Thread(target=_server.serve_forever)
+        thread.start()
+        yield queue
+    finally:
+        _server.shutdown()
+        thread.join()
+
+
+@pytest.fixture
+def client() -> Client:
+    return Client(MOCK_SERVER_URL, api_token=MOCK_SERVER_TOKEN)
+
+
+def test_get_dataset_status(client: Client) -> None:
+    dataset_id = 'fake-dataset-id'
+    workspace_id = 'fake-workspace-id'
+
+    expected_value = 'ok'
+
+    with _request_capture(f'{{"value": "{expected_value}"}}') as capture:
+        result = client.get_dataset_status(dataset_id, workspace_id)
+        assert result == expected_value
+
+        payload = capture.get()
+        assert payload.get('auth') == f'Bearer {MOCK_SERVER_TOKEN}'
+
+        assert dataset_id in payload.get('path')
+        assert workspace_id in payload.get('path')
+
+
+def test_get_model_status(client: Client) -> None:
+    model_id = 'fake-model-id'
+
+    expected_state = 'ok'
+
+    with _request_capture(f'{{"state": "{expected_state}"}}') as capture:
+        result = client.get_model_status(model_id)
+        assert result == expected_state
+
+        payload = capture.get()
+        assert payload.get('auth') == f'Bearer {MOCK_SERVER_TOKEN}'
+
+        assert model_id in payload.get('path')
+
+
+def test_get_deployment_status(client: Client) -> None:
+    deployment_id = 'fake-deployment-id'
+
+    with _request_capture(f'{{"oodStatus": "MAJOR"}}') as capture:
+        result = client.get_deployment_status(deployment_id)
+        assert result == dict(oodStatus='MAJOR')
+
+        payload = capture.get()
+        assert payload.get('auth') == f'Bearer {MOCK_SERVER_TOKEN}'
+
+        assert deployment_id in payload.get('path')
+
+
+def test_assign_model_to_deployment(client: Client) -> None:
+    model_id = 'fake-model-id'
+    deployment_id = 'fake-deployment-id'
+
+    with _request_capture() as capture:
+        client.assign_model_to_deployment(model_id, deployment_id)
+
+        payload = capture.get()
+        assert payload.get('auth') == f'Bearer {MOCK_SERVER_TOKEN}'
+
+        assert model_id in payload.get('path')
+        assert deployment_id in payload.get('path')
+
+
+def test_upload_model_zip(client: Client, tmp_path: Path) -> None:
+    use_case_id = 'fake-use-case-id'
+    workspace_id = 'fake-workspace-id'
+    model_name = 'fake-model-name'
+    model_content = 'fake-model-content'
+
+    path = tmp_path / 'model.zip'
+    with path.open('w') as file:
+        file.write(model_content)
+
+    with _request_capture(f'{{"id": "some-id"}}') as capture:
+        client.upload_model_zip(path, workspace_id, use_case_id, model_name)
+
+        payload = capture.get()
+        assert payload.get('auth') == f'Bearer {MOCK_SERVER_TOKEN}'
+
+        assert use_case_id in payload.get('path')
+        assert workspace_id in payload.get('path')
+
+        assert model_name.encode() in payload.get('content')
+        assert model_content.encode() in payload.get('content')
+
+
+def test_upload_csv(client: Client, tmp_path: Path) -> None:
+    workspace_id = 'fake-workspace-id'
+    csv_name = 'fake-csv-name'
+    csv_content = 'fake-csv-content'
+
+    path = tmp_path / 'data.csv'
+    with path.open('w') as file:
+        file.write(csv_content)
+
+    with _request_capture(f'{{"id": "some-id"}}') as capture:
+        client.upload_csv(path, workspace_id, csv_name)
+
+        payload = capture.get()
+        assert payload.get('auth') == f'Bearer {MOCK_SERVER_TOKEN}'
+
+        assert workspace_id in payload.get('path')
+
+        assert csv_name.encode() in payload.get('content')
+        assert csv_content.encode() in payload.get('content')
+
+
+def test_retrain_model(client: Client) -> None:
+    dataset_id = 'fake-dataset-id'
+    model_id = 'fake-model-id'
+    with _request_capture(f'{{"idOfNewModel": "some-id"}}') as capture:
+        client.retrain_model(model_id, dataset_id)
+        payload = capture.get()
+        assert payload.get('auth') == f'Bearer {MOCK_SERVER_TOKEN}'
+        assert model_id in payload.get('path')
+        assert dataset_id.encode() in payload.get('content')
