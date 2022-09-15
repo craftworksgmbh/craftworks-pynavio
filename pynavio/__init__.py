07cf786f73986494713e9b6dcfa3f4e0ad6b7d75
tatevik.gharagyozyan <tatevik.gharagyozyan@craftworks.at>
2022-09-15 16:35:24 +0200

    Bump version: 0.1.5 → 0.1.6

--- pynavio/__init__.py
+++ pynavio/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """craftworks"""
 __email__ = 'dev-accounts@craftworks.at'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 from . import _code as code
 from . import _mlflow as mlflow
