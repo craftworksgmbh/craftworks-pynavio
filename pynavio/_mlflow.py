7750d6faa89ae2bdae859905753c74aecdea423f
tatevik.gharagyozyan <tatevik.gharagyozyan@craftworks.at>
2022-11-30 16:55:26 +0100

    refactor(pynavio._mlflow): moves the schemas to utils.schemas

--- pynavio/_mlflow.py
+++ pynavio/_mlflow.py
@@ -16,76 +16,14 @@ import jsonschema
 
 from pynavio.utils import ExampleRequestType, make_env
 from pynavio.utils.json_encoder import JSONEncoder
+from pynavio.utils.schemas import PREDICTION_SCHEMA, \
+    METADATA_SCHEMA, REQUEST_SCHEMA_SCHEMA
 
 EXAMPLE_REQUEST = 'example_request'
 ARTIFACTS = 'artifacts'
 ArtifactsType = Optional[Dict[str, str]]
 ERROR_KEYS = {'error_code', 'message', 'stack_trace'}
 PREDICTION_KEY = 'prediction'
-
-METADATA_SCHEMA = {
-    'type': 'object',
-    'properties': {
-        'request_schema': {
-            'type': 'object',
-            'properties': {
-                'path': {'type': 'string'}
-            }
-        },
-        'dataset': {
-            'type': 'object',
-            'properties': {
-                'name': {'type': 'string'},
-                'path': {'type': 'string'}
-            }
-        },
-        'oodDetection': {
-            'type': 'string',
-            'enum': ['disabled', 'default']
-        },
-        'explanations': {
-            'type': 'string',
-            'enum': ['disabled', 'default', 'plotly']
-        },
-        '.shm_mount': {'type': 'string'}
-    },
-    'required': ['request_schema']
-}
-
-COLUMN_SCHEMA = {
-    'type': 'object',
-    'properties': {
-        'name': {'type': 'string'},
-        'sampleData': {'type': [
-            'boolean', 'integer', 'number', 'array', 'string'
-        ]},
-        'type': {
-            'type': 'string',
-            'enum': ['float', 'string', 'image', 'list', 'bool', 'int',
-                     'timestamp']
-        },
-        'nullable': {'type': 'boolean'}
-    },
-    'required': ['name', 'sampleData', 'type', 'nullable']
-}
-
-REQUEST_SCHEMA_SCHEMA = {
-    'type': 'object',
-    'properties': {
-        'featureColumns': {
-            'minItems': 1,
-            'type': 'array',
-            'items': COLUMN_SCHEMA
-        },
-        'targetColumns': {
-            'minItems': 1,
-            'type': 'array',
-            'items': COLUMN_SCHEMA
-        },
-        'dateTimeColumn': COLUMN_SCHEMA
-    },
-    'required': ['featureColumns', 'targetColumns']
-}
 
 
 def _get_field(yml: dict, path: str) -> Optional[Any]:
@@ -318,35 +256,8 @@ class _ModelValidator:
     @staticmethod
     def verify_model_output(model_output, **kwargs):
         def _validate_prediction_schema(model_prediction):
-            not_sequence_prediction_types = ['boolean', 'integer',
-                                             'number', 'string']
-
-            prediction_schema = {
-                "type": "object",
-                "properties": {
-                    PREDICTION_KEY: {"oneOf":
-                                     [
-                                      {"type": not_sequence_prediction_types},
-                                      {"type": 'array',
-                                       "minItems": 1,
-                                       "items": {"type": "boolean"},
-                                       },
-                                      {"type": 'array',
-                                          "minItems": 1,
-                                          "items": {"type": "number"},
-                                       },
-                                      {"type": 'array',
-                                       "minItems": 1,
-                                       "items": {"type": "string"},
-                                       },
-                                     ]
-                                     },
-                },
-                "required": [PREDICTION_KEY],
-            }
-
             try:
-                jsonschema.validate(model_prediction, prediction_schema)
+                jsonschema.validate(model_prediction, PREDICTION_SCHEMA)
             except jsonschema.exceptions.ValidationError:
                 print(f"Error: The value of model_output['{PREDICTION_KEY}']"
                       " must be one of the following types "
