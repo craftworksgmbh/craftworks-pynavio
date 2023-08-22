42963e117bcfaafce07ecabaf647ce447a7371f7
tatevik.gharagyozyan <tatevik.gharagyozyan@craftworks.at>
2023-08-22 14:49:22 +0200

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/craftworksgmbh/craftworks-pynavio',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 
 )
