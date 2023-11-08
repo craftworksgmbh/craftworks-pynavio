19eee4f47e89c39de63ab31a26313e9b94ffdfb7
clarareolid <clara.reolid-sanchez@craftworks.at>
2023-11-08 10:18:59 +0100

    tests conda v1

--- tests/test_pynavio/test_utils/test_conda.py
+++ tests/test_pynavio/test_utils/test_conda.py
@@ -27,42 +27,51 @@ def test_make_conda_env_positive_yaml(args, expected=yaml_path):
     assert conda_env == expected
 
 
-@pytest.mark.parametrize("args, expected", [
-    ({
-        'pip_packages': ['numpy', 'mlflow']
-    }, {
-        'channels': ['defaults', 'conda-forge'],
-        'dependencies': [
-            f'python={platform.python_version()}', f'pip={pip.__version__}', {
-                'pip': ['numpy', 'mlflow']
-            }
-        ],
-        'name': 'venv'
-    }),
-    ({
-        'pip_packages': ['numpy==1.20.2', 'mlflow']
-    }, {
-        'channels': ['defaults', 'conda-forge'],
-        'dependencies': [
-            f'python={platform.python_version()}', f'pip={pip.__version__}', {
-                'pip': ['numpy==1.20.2', 'mlflow']
-            }
-        ],
-        'name': 'venv'
-    }),
-    ({
-        'pip_packages': ['numpy', 'mlflow'],
-        'conda_channels': ['pytorch']
-    }, {
-        'channels': ['defaults', 'conda-forge', 'pytorch'],
-        'dependencies': [
-            f'python={platform.python_version()}', f'pip={pip.__version__}', {
-                'pip': ['numpy', 'mlflow']
-            }
-        ],
-        'name': 'venv'
-    }),
-])
+@pytest.mark.parametrize(
+    "args, expected",
+    [
+        ({
+            'pip_packages': ['numpy', 'mlflow']
+        }, {
+            'channels': ['defaults', 'conda-forge'],
+            'dependencies': [
+                f'python={platform.python_version()}',
+                f'pip={pip.__version__}', {
+                    'pip': ['numpy', 'mlflow']
+                }
+            ],
+            'name': 'venv'
+        }),
+        ({
+            'pip_packages': ['numpy==1.20.2', 'mlflow']
+        }, {
+            'channels': ['defaults', 'conda-forge'],
+            'dependencies': [
+                f'python={platform.python_version()}',
+                f'pip={pip.__version__}', {
+                    'pip': ['numpy==1.20.2', 'mlflow']
+                }
+            ],
+            'name': 'venv'
+        }),
+        ({
+            'pip_packages': ['numpy', 'mlflow'],
+            'conda_channels': ['pytorch']
+        }, {
+            'channels': ['defaults', 'conda-forge', 'pytorch'],
+            'dependencies': [
+                f'python={platform.python_version()}',
+                f'pip={pip.__version__}', {
+                    'pip': ['numpy', 'mlflow']
+                }
+            ],
+            'name': 'venv'
+        }),
+        ({
+            'pip_packages': None,
+            'conda_env': None
+        }, None),
+    ])
 def test_make_conda_env_positive(args, expected):
     conda_env = make_env(**args)
     assert conda_env == expected
