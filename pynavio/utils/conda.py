afcad9bbc3aaa917342ab05d23435c9950cdb84f
clarareolid <127108703+clarareolid@users.noreply.github.com>
2023-11-09 09:13:01 +0100

    Update conda.py
    Add docstring usage information

--- pynavio/utils/conda.py
+++ pynavio/utils/conda.py
@@ -12,6 +12,8 @@ def make_env(
 ) -> Dict[str, Any]:
     """
     makes the value for the mlflow.pyfunc.save_model()'s conda_env argument
+    Usage: either pip_packages or conda_env need to be set. If both are not set
+    then the env will be inferred by mlflow.
 
     @param pip_packages:
     @param conda_packages:
