8a8a69e257d9722b44ef45dd6a5031e99a89ab1a
clarareolid <clara.reolid-sanchez@craftworks.at>
2024-01-22 11:36:08 +0100

    Bump version: 0.2.4 → 0.3.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/craftworksgmbh/craftworks-pynavio',
-    version='0.2.4',
+    version='0.3.0',
     zip_safe=False,
 
 )
