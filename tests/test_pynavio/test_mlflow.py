007d27ee0dd5122a48721796080fe0a4616829cb
Michael Raffelsberger <michael.raffelsberger@craftworks.at>
2024-12-30 18:44:50 +0100

    test(test_mlflow): add metadata unit tests

--- tests/test_pynavio/test_mlflow.py
+++ tests/test_pynavio/test_mlflow.py
@@ -151,7 +151,7 @@ def test_is_input_nested(fixtures_path, schema_file_name, is_nested):
            == is_nested
 
 
-def test__add_sys_dependencies(tmp_path):
+def test_add_sys_dependencies(tmp_path):
     pynavio.mlflow._add_sys_dependencies(tmp_path, ["lib1", "lib2"])
     file_path = tmp_path / 'sys_dependencies.txt'
 
@@ -160,12 +160,12 @@ def test__add_sys_dependencies(tmp_path):
         assert file_content == "lib1\nlib2"
 
 
-def test__add_sys_dependencies_fails_on_str(tmp_path):
+def test_add_sys_dependencies_fails_on_str(tmp_path):
     with pytest.raises(AssertionError):
         pynavio.mlflow._add_sys_dependencies(tmp_path, "lib1")
 
 
-def test__add_sys_dependencies_no_resulting_file(tmp_path):
+def test_add_sys_dependencies_no_resulting_file(tmp_path):
     import os
     pynavio.mlflow._add_sys_dependencies(tmp_path, None)
     file_path = tmp_path / 'sys_dependencies.txt'
@@ -173,7 +173,7 @@ def test__add_sys_dependencies_no_resulting_file(tmp_path):
     assert not os.path.exists(file_path)
 
 
-def test__is_wrapped_by_prediction_call():
+def test_is_wrapped_by_prediction_call():
 
     def predict():
         pass
@@ -320,3 +320,81 @@ def test_to_navio_extra_dependencies(tmp_path, extra_pip_packages,
                      conda_env)
     except Exception:
         raise pytest.fail("Unexpected Exception")
+
+
+@pytest.mark.parametrize("to_navio_kwargs, metadata", [
+    (
+        {
+            "metadata": {"key1": "value1", "key2": "value2"},
+            "num_gpus": 2,
+        },
+        {
+            "key1": "value1",
+            "key2": "value2",
+            "request_schema": {"path": "artifacts/example_request.json"},
+            "explanations": "default",
+            "oodDetection": "default",
+            "gpus": 2,
+        }
+    ),
+    (
+        {
+            "metadata": None,
+            "num_gpus": 1,
+        },
+        {
+            "request_schema": {"path": "artifacts/example_request.json"},
+            "explanations": "default",
+            "oodDetection": "default",
+            "gpus": 1,
+        }
+    )
+])
+def test_to_navio_metadata(tmp_path, to_navio_kwargs, metadata):
+    import yaml
+    import mlflow
+    import pynavio
+
+    from tempfile import TemporaryDirectory
+    from pathlib import Path
+
+    # Specify example request
+    TARGET = 'target'
+    _columns = ['x', 'y']
+    example_request = pynavio.make_example_request(
+        data={
+            TARGET: float(sum(range(len(_columns)))),
+            **{col: float(i) for i, col in enumerate(_columns)}
+        },
+        target=TARGET
+    )
+
+    # Create dummy model
+    class SampleModel(mlflow.pyfunc.PythonModel):
+
+        @pynavio.prediction_call
+        def predict(self, context, model_input):
+            return {
+                'prediction': [1.] * model_input.shape[0]
+            }
+
+    # Set model path
+    model_path = str(tmp_path / 'model')
+
+    # Setup model
+    with TemporaryDirectory():
+        pynavio.mlflow.to_navio(
+            model=SampleModel(),
+            path=model_path,
+            example_request=example_request,
+            pip_packages=['mlflow'],
+            **to_navio_kwargs,
+
+        )
+
+        path = Path(model_path) / 'MLmodel'
+        with path.open('r') as file:
+            cfg = yaml.safe_load(file)
+
+    # Assert correct metadata
+    assert cfg["metadata"] == metadata
